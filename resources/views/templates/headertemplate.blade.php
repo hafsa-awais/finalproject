e801f0d1f697d7301c3bdabce03c88eda0fb0420
Hafsa <haffi.kh@gmail.com>
2022-06-14 14:02:25 +0200

    header in process

--- resources/views/templates/headertemplate.blade.php
+++ resources/views/templates/headertemplate.blade.php
@@ -6,22 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body>
-    <nav>
-        <ul>
-            <li>
-                <a href="/movies">Login</a>
-            </li>
-            <li>
-                <a href="/contact">Register</a>
-            </li>
-        </ul>
-    </nav>
-
-    <img src="../css/assets/Logo.png">
+<nav class="flex align-center">
+    <img src="../../css/assets/style.css
+  <p><span>at</span>Event</p>
+  <ul>
+    <li>
+      <a href="#">Home</a>
+      <a href="#">About Us</a>
+      <button class="btn register">Register</button>
+      <button class="btn login">Log In</button>
+    </li>
+  </ul>
+</nav>
 
 </body>
 

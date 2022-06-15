dda32c1bcf931e754e2dd32318168c8b205b5524
Griha <gorshenko.d@gmail.com>
2022-06-15 11:24:41 +0200

    footer

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,3 +4,4 @@
 
 @section('content')
 
+@extends('mytemplatefooter')
---
+++ resources/views/mytemplatefooter.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/stylefooter.css" rel="stylesheet" />
+</head>
+<div class="footer">
+    <div id="button"></div>
+    <div class="contener">
+
+        <div class="grid">
+
+            <div class="box-init" id="logo">
+                <div id="logo">
+                    <img src="/css/logo.png">
+                </div>
+            </div>
+
+
+
+
+            <div class="box-init">
+               <a href="#.">About Us</a>
+            </div>
+
+            <div class="box-init">
+               <a href="#.">Contact</a>
+            </div>
+
+            <div class="box-init">
+               <a href="#.">Advertisers</a>
+            </div>
+
+
+            <div class="box-init">
+               <a href="#.">Carrieres</a>
+            </div>
+
+            <div class="box-init">
+               <a href="#.">Responsibility</a>
+            </div>
+
+
+            <div class="box-init">
+            <a href="mailto:gorshenko.d@gmail.com" class="button">send a message</a>
+            </div>
+
+
+
+            
+
+        </div>
+
+    </div>
+
+</div>
+</div>
+</body>
+
+</html>

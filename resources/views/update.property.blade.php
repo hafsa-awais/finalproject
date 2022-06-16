88860281cafeb44efd73cd79bc44e2fd8eb7fbbb
venus <agrawal.venus@gmail.com>
2022-06-16 14:11:15 +0200

    new and updates forms

---
+++ resources/views/update.property.blade.php
@@ -0,0 +1,13 @@
+<h2>Add a new flower</h2>
+<form action="" method="post">
+    @csrf
+    @method('PUT')
+    <input type="text" name="name"placeholder="name" value="{{$flower->name}}"><br>
+    <input type="text" name="poster"placeholder="picture" value="{{$flower->poster}}"><br>
+    <!--target the update inserting for removing the 'euro' sign inside the form while updating the form--> 
+    <input type="text" name="price"placeholder="price" value="{{$flower->getAttributes()['price']}}"><br>
+    <input type="text" name="description"placeholder="description" value="{{$flower->description}}"><br>
+    <input type="text" name="type"placeholder="type" value="{{$flower->type}}"><br>
+    <input type="submit" value="Update">    
+</form>
+@endsection

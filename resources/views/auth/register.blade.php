96d7606f3bf0e9d94f377c2a852f016413060614
Hafsa <haffi.kh@gmail.com>
2022-06-20 10:54:12 +0200

    somestuff

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registration form</title>
-</head>
-<body>
+@extends('templates/headerAndFooterTemplate')
+
+
+@section('title', 'Registeration Page')
+
+
+@section('content')
     @if($errors->any())
 <div class="alert danger">
     @foreach($errors->all() as $error)
     <li style="color: red"> {{$error}}</li>
     @endforeach
+</div>
 @endif
 
-<h2>Registration Form</h2>
-<form action="" method="post" id="myForm">
+
+<!-- <form action="" method="post" id="myForm">
     @csrf
     <input type="text" name="first_name"placeholder="firstname"><br>
     <input type="text" name="last_name"placeholder="lastname"><br>
@@ -25,6 +24,105 @@
     <input type="text" name="password"placeholder="password"><br>
     <input type="text" name="password_confirmation"placeholder="Confirm password"><br> 
     <input type="submit" value="Submit"> 
-</form>
-</body>
-</html>
+</form> -->
+
+
+<section class="vh-50" style="background-color: #eee;padding-top: 50px; padding-bottom: 50px;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                <form class="mx-1 mx-md-4" action="" method="post" id="myForm">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="first_name" id="form3Example1c" class="form-control" />
+                      <label class="form-label" for="form3Example1c">Firstname</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                  <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="email" name="last_name" id="form3Example3c" class="form-control" />
+                      <label class="form-label" for="form3Example3c">Lastname</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                  <i class="fas fa-lg me-3 fa-map-marker-alt fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="address" id="form3Example4c" class="form-control" />
+                      <label class="form-label" for="form3Example4c">Address</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                  <i class="fas fa-lg  me-3 fa-phone fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="contact" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Phone Number</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                  <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="email" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Email</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                  <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="password" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" name="password_confirmation" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Confirm password</label>
+                    </div>
+                  </div>
+
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <label class="form-check-label" for="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" class="btn btn-success btn-lg">Register</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                  class="img-fluid" alt="Sample image">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+@endsection
